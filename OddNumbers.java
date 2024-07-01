11e3c3469be21672e84aa89ad04f73673075abea
Tshirisha <147329845+Tshirisha@users.noreply.github.com>
2024-07-01 11:55:18 +0530

    Add files via upload

---
+++ OddNumbers.java
@@ -0,0 +1,12 @@
+public class OddNumbers {
+    public static void main(String args[]){
+        int X=25;
+        System.out.println("Odd Numbers from 1 to"+X+":");
+        for(int i=1;i<=X;i++){
+            if(i%2!=0){
+                System.out.println(i + "");
+
+            }
+        }
+    }
+}
