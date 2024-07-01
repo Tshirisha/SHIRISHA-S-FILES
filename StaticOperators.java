994fbd9714ec00bf80dd8ca522247fe80422c765
Tshirisha <147329845+Tshirisha@users.noreply.github.com>
2024-07-01 12:14:21 +0530

    Add files via upload

---
+++ StaticOperators.java
@@ -0,0 +1,7 @@
+public class StaticOperators{
+    public static void main(String args[]){
+        System.out.println(StaticOp.addition(20,30));
+        System.out.println(StaticOp.subtraction(20,30));
+        System.out.println(StaticOp.multiplication(20,30));
+    }
+}
