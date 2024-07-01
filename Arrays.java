b41e9aa7fe9d9a8596aad55b779c6674c0290aba
Tshirisha <147329845+Tshirisha@users.noreply.github.com>
2024-07-01 12:09:32 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,21 @@
+public class Arrays{
+    public static void main(String aargs[]){
+        int x[]=new int[10];
+        x[0]=1;
+        x[1]=2;
+        x[2]=3;
+        x[3]=4;
+        x[4]=5;
+        x[5]=6;
+        x[6]=7;
+        x[7]=8;
+        x[8]=9;
+        x[9]=10;
+        System.out.println(x.length);
+        for(int i =0; i < x.length; i++)
+        System.out.println(x[i]);
+
+
+
+    }
+}
