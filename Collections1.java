a16f5943d63169c5219e26143deffa54523357d4
Tshirisha <147329845+Tshirisha@users.noreply.github.com>
2024-07-01 12:16:15 +0530

    Add files via upload

---
+++ Collections1.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Collections1{
+    public static void main(String args[]){
+        ArrayList a2=new ArrayList();
+        a2.add("shirisha");
+        a2.add(90);
+        a2.add(989898L);
+        a2.add(new Integer(60));
+            System.out.println(a2);
+        }
+    }
