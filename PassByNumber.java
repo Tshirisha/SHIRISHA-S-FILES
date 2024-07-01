4e4af2b6705614a4ef686703e61c6adf91ffe50f
Tshirisha <147329845+Tshirisha@users.noreply.github.com>
2024-07-01 12:08:38 +0530

    Add files via upload

---
+++ PassByNumber.java
@@ -0,0 +1,12 @@
+public class PassByNumber{
+    public static void main(String args[]){
+        int x=1;
+        System.out.println(x);
+        int y=2;
+        System.out.println(y);
+        y=30;
+        System.out.println(y);
+        System.out.println(x);
+
+    }
+}
