8920ddbb43c627032396488e7e7ae1f4dcc6fe97
Tshirisha <147329845+Tshirisha@users.noreply.github.com>
2024-07-01 12:17:58 +0530

    Add files via upload

---
+++ MapExamples1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class MapExamples1
+{
+    public static void main(String[] args)
+    {
+        Map<Character,String> map=new HashMap<Character,String>();
+        map.put('a',"siri");
+        map.put('b',"anu");
+        map.put('c',"yashu");
+        map.put('d',"gani");
+        map.put('e',"minnu");
+        System.out.println(map);
+    }
+}
