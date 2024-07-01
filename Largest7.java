75af3673fffdc60056a5a80fc747c7369cf7f113
Tshirisha <147329845+Tshirisha@users.noreply.github.com>
2024-07-01 11:47:24 +0530

    Add files via upload

---
+++ Largest7.java
@@ -0,0 +1,13 @@
+public class Largest7 {
+    public static void main(String args[]){
+        Long a=9898981234l,b=-9898981234l,c=8765434444l;
+if(a>=b &&a>=c)
+System.out.println(a +"is the largest number.");
+else if(b>=a &&b>=c)
+System.out.println(b +"is the largest number.");
+else 
+System.out.println(c+ "is the largest number" );
+
+    }
+    
+}
