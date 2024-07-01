37c2e8f8f9daa0d42dee4ea4666336d9f82e3ef0
Tshirisha <147329845+Tshirisha@users.noreply.github.com>
2024-07-01 11:58:19 +0530

    Add files via upload

---
+++ Squares1.java
@@ -0,0 +1,10 @@
+public class Squares1 {
+    public static void main(String args[]){
+        System.out.println("Squares Numbers Series: " );
+        for(int i=2;i<=10;i++){
+           int square=i*2;
+            System.out.println(square +"");
+
+        }
+    }
+}
