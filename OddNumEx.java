51e12f2eaf2852d1273f1e0219f453f8c43a49cf
Tshirisha <147329845+Tshirisha@users.noreply.github.com>
2024-07-01 12:13:46 +0530

    Add files via upload

---
+++ OddNumEx.java
@@ -0,0 +1,17 @@
+public class OddNumEx
+{
+    public static void main(String args[])
+    {
+        int number=20;
+        System.out.println("odd numbers from 10 to"+number+":-");
+        for(int i=10;i<=number;i++)
+        {
+         if(i%2!=0)
+
+         {
+            System.out.println(i+"");
+    
+         }
+        }
+    }
+}
