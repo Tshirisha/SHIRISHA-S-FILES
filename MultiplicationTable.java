0c66df23fbf6cbefb51f61cd5f090e12bda3fd70
Tshirisha <147329845+Tshirisha@users.noreply.github.com>
2024-07-01 11:52:25 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,8 @@
+public class MultiplicationTable {
+    public static void main(String args[]){
+        int x = 5;
+for (int i=0;i<=20;i++){
+    System.out.println(x +"*"+ i + "=" + x*i);
+}
+    }
+}
