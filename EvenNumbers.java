ff3431bca540cb9ae4ed341c2c1fdaab9fdf0321
Tshirisha <147329845+Tshirisha@users.noreply.github.com>
2024-07-01 11:54:49 +0530

    Add files via upload

---
+++ EvenNumbers.java
@@ -0,0 +1,13 @@
+public class EvenNumbers {
+    public static void main(String args[]){
+        int X=25;
+        System.out.println("Even Numbers from 1 to"+X+".");
+        for(int i=1;i<=X;i++){
+            if(i%2==0){
+                System.out.println(i +"");
+                
+            }
+        }
+
+    }
+}
